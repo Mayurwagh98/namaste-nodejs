176a688def5ff048d6c723e0486ae9e35fefe1d2
mayur wagh <mswagh98@gmail.com>
2025-02-08 16:21:32 +0530

    Added: sync code

--- sync_async_code/async.js
+++ sync_async_code/async.js
@@ -6,14 +6,21 @@ console.log("hello world");
 let a = 2321;
 let b = 43434;
 
+// sync
+fs.readFileSync("./file.txt", "utf-8");
+console.log("geting sync data");
+
+// async code
 https.get("https://dummyjson.com/products/1", (res) => {
   console.log("fetched data successfully");
 });
 
+// async
 setTimeout(() => {
   console.log("setTimeout called after 5 seconds");
 }, 5000);
 
+// async
 fs.readFile("./file.txt", "utf-8", (err, data) => {
   console.log("file data", data);
 });
